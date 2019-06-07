9dbf5e5ce42b0e5650b48f3e597e7bffc8789077
Steven Venham <deathmock@gmail.com>
2019-06-06 22:29:51 -0400

    ;-; Compete rewrite

--- BattleBot-Arena.ino
+++ BattleBot-Arena.ino
@@ -1,161 +1,126 @@
-    const int P1Pin =4;
-    const int P1Light=8;
-    const int P2Pin =2;
-    const int P2Light=9;
-    const int RefPin =3;
-    const int RefLight=10;
-    const int MainLightPin = 11;
-    const int TrapDoor1Pin1 =12;   //7654
-    const int TrapDoor1Pin2 =13;
-    const int TrapDoor2Pin1 =34;
-    const int TrapDoor2Pin2 =36;
+    #include <Adafruit_NeoPixel.h>
     
-    const int TrapSensor1 = 30;
-    const int TrapSensor2 = 32;
-
-    const int MatchSecondsPart1 = 60;
-    const int MatchSecondsPart2 = 120;
     
-    boolean Player1Ready = false;
-    boolean Player2Ready = false;
-    boolean StartRound   = false;
-    boolean Trap1Ready = false;
-    boolean Trap2Ready = false;
+    /*  PINOUT  */
+    //0 tx
+    //1 rx
+    const uint8_t LIGHTPIN = 2;
+    //3 unused
+    const uint8_t RINGRPIN = 4;
+    const uint8_t DOORPPIN = 5;
+    const uint8_t DOOR2PIN = 6;
+    const uint8_t DOOR1PIN = 7;
+    const uint8_t BUTN1PIN = 8;
+    const uint8_t BUTN2PIN = 9;
+    const uint8_t DOORSEN1 = 10;
+    const uint8_t DOORSEN2 = 11;
 
-    int thousand=0;
+    //Player states
+    bool player1ready = false;
+    bool player2ready = false;
 
-void openTrapDoor(const int door){
-  digitalWrite(doorp,HIGH); //Energise Door
-  digitalWrite(door,HIGH);
-  delay(500);
-  digitalWrite(door,LOW);
-  digitalWrite(doorp,LOW); //
+    //Round Stuff
+    const long ROUNDLENGHT =  180000; //3 Minutes
+    const long DOOROPENTIME = 120000; //2 Minutes
+    long currentroundtime = 0;
+    
+    //Door Stuff
+    bool door1open = false;
+    bool door2open = false;
+
+    //Light Stuff
+    typedef uint32_t Color;
+    Adafruit_NeoPixel strip = Adafruit_NeoPixel(60, LIGHTPIN, NEO_GRB + NEO_KHZ800);
+    Color RED = strip.Color(255, 0, 0);
+    Color YELLOW = strip.Color(255,255,0);
+    Color GREEN = strip.Color(255, 0, 0);
+    
+    //Functions
+void ringBell(){
+  digitalWrite(RINGRPIN,HIGH);
+  delay(1000);
+  digitalWrite(RINGRPIN,LOW);
 }
 
-void openTrapDoor1(){
-    digitalWrite(TrapDoor1Pin2,LOW);
-    delay(500);
-    digitalWrite(TrapDoor1Pin1,HIGH);
-    digitalWrite(TrapDoor1Pin2,HIGH);
-  }//openTrapDoor()
-void closeTrapDoor1(){
-    digitalWrite(TrapDoor1Pin1,LOW);
-    delay(500);
-    digitalWrite(TrapDoor1Pin1,HIGH);
-    digitalWrite(TrapDoor1Pin2,HIGH);
-  }//closeTrapDoor1
-void openTrapDoor2(){
-    digitalWrite(TrapDoor2Pin2,LOW);
-    delay(500);
-    digitalWrite(TrapDoor2Pin1,HIGH);
-    digitalWrite(TrapDoor2Pin2,HIGH);
-  }//openTrapDoor2()
-void closeTrapDoor2(){
-    digitalWrite(TrapDoor2Pin1,LOW);
-    delay(500);
-    digitalWrite(TrapDoor2Pin1,HIGH);
-    digitalWrite(TrapDoor2Pin2,HIGH);
-  }//closeTrapDoor2
+void doCountdownLights(){
+  
+}
 
-void setMainLights(boolean inState) {
-    digitalWrite(MainLightPin,inState?HIGH:LOW);
-  }//SetMainLights()
+void checkPlayer(const uint8_t PIN,bool& isready){
+  if(isready){
+    return;
+  }
+  if(digitalRead(PIN) == HIGH){
+    //Set Lights
+    isready = true;
+  }
+}
 
-void setPlayerLights(int player , boolean inState) {
-    digitalWrite( (player==1)?P1Light:P2Light,inState?HIGH:LOW);  
-  }//setPlayerLights()
+void openTrapDoors(){
+  digitalWrite(DOORPPIN,HIGH); //Energise Door
+  delay(500);
+  digitalWrite(DOORPPIN,LOW); //
+}
 
-void setRefLight(boolean inState ) {
-    digitalWrite(RefLight,inState?HIGH:LOW);
-  }//setRefLight()
+bool closeTrapDoor(const uint8_t door,const uint8_t sensor,bool& closed){
+  if(closed){
+    return true;
+  }
+  if(digitalRead(sensor) == HIGH){
+    //It just closed
+    delay(10);
+    digitalWrite(door,HIGH);
+    delay(100);
+    digitalWrite(door,LOW);
+    closed = true;
+  }
+  //Still open
+}
+
+void closeTrapDoors(){
+  while(door1open || door2open){
+    closeTrapDoor(DOOR1PIN,DOORSEN1,door1open);
+    closeTrapDoor(DOOR2PIN,DOORSEN2,door2open);
+  }
+}
 
 void setup() {
-  pinMode(P1Pin,INPUT_PULLUP);
-  pinMode(P2Pin,INPUT_PULLUP);
-  pinMode(RefPin,INPUT_PULLUP);
-  pinMode(P1Light,OUTPUT);
-  pinMode(P2Light,OUTPUT);
-  pinMode(RefLight,OUTPUT);
-  pinMode(MainLightPin,OUTPUT);
-  pinMode(TrapDoorPin1,OUTPUT);
-  pinMode(TrapDoorPin2,OUTPUT);
+  pinMode(LIGHTPIN,OUTPUT);
+  pinMode(RINGRPIN,OUTPUT);
+  pinMode(DOORPPIN,OUTPUT);
+  pinMode(DOOR2PIN,OUTPUT);
+  pinMode(DOOR1PIN,OUTPUT);
+  pinMode(BUTN1PIN,INPUT_PULLUP);
+  pinMode(BUTN2PIN,INPUT_PULLUP);
+  pinMode(DOORSEN1,INPUT_PULLUP);
+  pinMode(DOORSEN2,INPUT_PULLUP);
 
-  
-  setMainLights(false);
-  setPlayerLights(1,false);
-  setPlayerLights(2,false);
-  setRefLight(false);
-  closeTrapDoor();
-  Serial.begin(9600);
-}//setup
+  digitalWrite(RINGRPIN,LOW);
+  digitalWrite(DOORPPIN,LOW);
+  digitalWrite(DOOR1PIN,LOW);
+  digitalWrite(DOOR2PIN,LOW);
+  strip.begin();
+  strip.show();
+  closeTrapDoors(); //Ensure doors are closed before game begins.
+}
 
 void loop() {
-  
-  while ( (!Player1Ready) || (!Player2Ready)) {
-      if (digitalRead(P1Pin) == HIGH) {
-          Serial.println("Player 1 is pushing button");
-          Player1Ready= true;
-          setPlayerLights(1,true);
-      }//if
-      if (digitalRead(P2Pin) == HIGH) {
-          Serial.println("Player 2 is pushing button");
-          Player2Ready= true;
-          setPlayerLights(2,true);
-      }//if
-  
-    thousand++; if (thousand>1000) {thousand=0;}
-    
-    Serial.print  (Player1Ready?"Y":"N" );
-    Serial.println(Player2Ready?"Y":"N" );
-    
-  }//while
-  Serial.println("Both Players are ready, awaiting Ref:");
-  while ( ! StartRound  ) {
-    if (digitalRead(RefPin)==HIGH){
-      Serial.println("The Ref is pushing button");
-      StartRound= true;
-      setRefLight(true);
-    }  //if
-  }//while
-  Serial.println("Ref is ready, starting match...");
-  setMainLights(true);
-  
-  for(int x=0;x<MatchSecondsPart1;x++){
-        delay(100);     //1000
-        Serial.println(x);
-        //do something cool?
-      }//for
-
-openTrapDoor(); Serial.println("Trap Door open");
-  
-  for(int x=0;x<MatchSecondsPart1;x++){
-        delay(100);//1000
-        Serial.println(x);
-        //do something cool?     
-      }//for
-
-
-Serial.println("Round Over, waiting for Ref to reset");
-  while (digitalRead(RefPin)==LOW) {}//Wait for someone to press the ref button to reset
-
-  while ( (!Trap1Ready) || (!Trap2Ready) ) {
-      if digitalRead(TrapSensor1==LOW) {
-            Trap1Ready = true;
-            closeTrapDoor1();
-          }
-      if digitalRead(TrapSensor2==LOW) {
-            Trap2Ready = true;
-            closeTrapDoor2();
-          }
+  if(currentroundtime != 0){              //We are in a round
+    delay(1);
+    currentroundtime++;
+    if(currentroundtime == DOOROPENTIME){ //Time to open the doors.
+      openTrapDoors();
     }
-    
-    setPlayerLights(1,false);
-    Player1Ready=false;
-    setPlayerLights(2,false);
-    Player2Ready=false;
-    setRefLight(false);
-    StartRound=false;
-    closeTrapDoor();
-Serial.println("Arena Reset");
-
-}//loop
+    if(currentroundtime == DOOROPENTIME){ //Round end
+      ringBell();
+    }
+  }
+  else {                                  //We are not in a round.
+    checkPlayer(BUTN1PIN,player1ready);
+    checkPlayer(BUTN2PIN,player2ready);
+    if(player1ready && player2ready){
+        doCountdownLights();
+        currentroundtime = 1;
+    }
+  }
+}
